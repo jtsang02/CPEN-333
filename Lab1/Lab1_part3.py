4eb1cd378232b7d8a22bc1db5449711c6563ae12
jtsang02 <jtsang02@student.ubc.ca>
2022-01-19 23:32:43 -0800

    Create Lab1_part3.py

---
+++ Lab1/Lab1_part3.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.pensize(7)
+colors = ['black', 'blue','red', 'yellow', 'green']             # list of different colors
+coordinates = [(0,0), (-100,0), (100,0), (-50,-50), (50, -50)]  #coordinates of each ring
+for i in range(5):
+    turtle.goto(coordinates[i]) # set coordinate
+    turtle.pendown()            # pen down to draw circle
+    turtle.color(colors[i])     # set color
+    turtle.circle(45)           # draw circle of size r=45
+    turtle.penup()              # pen up to move coordinates
+
+turtle.hideturtle()
+turtle.done()
