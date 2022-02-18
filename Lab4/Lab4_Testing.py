258fda75f2666f375ef248d85dd5a6320640bc12
jtsang02 <jtsang02@student.ubc.ca>
2022-02-18 03:19:37 -0800

    lab4 unittest

--- Lab4/Lab4_Testing.py
+++ Lab4/Lab4_Testing.py
@@ -5,15 +5,19 @@ import threading
 import unittest
 
 class TestLab4:
-    
+
+    testcase: list = []
     sortedFirstHalf: list = []
     sortedSecondHalf: list = []
-    SortedFullList: list = []
+    sortedFullList: list = []
 
-    def __init__(self, testCase: list):
+    def __init__(self, passedList: list):
         #shared variables
-        self.testcase = testCase
-
+        self.testcase = passedList
+        TestLab4.sortedFirstHalf.clear()
+        TestLab4.sortedSecondHalf.clear()
+        TestLab4.sortedFullList.clear()
+    
     def sortingWorker(self, firstHalf: bool) -> None:
         """
         If param firstHalf is True, the method
@@ -35,7 +39,7 @@ class TestLab4:
                     j -= 1                                  # decrement loop variable
                 listToSort[j + 1] = current                 # after while loop, all greater items shifted to right, store current item
 
-        tempList = self.testcase                        # copy testcase to new list        
+        tempList = self.testcase                            # copy testcase to new list        
         midIndex = len(tempList) // 2                       # get midpoint index of list
             
         if firstHalf:
@@ -47,10 +51,9 @@ class TestLab4:
 
         for item in tempList:                               
             if firstHalf:                                   # add to appropriate shared variable
-                self.sortedFirstHalf.append(item)      
+                TestLab4.sortedFirstHalf.append(item)      
             else:
-                self.sortedSecondHalf.append(item)
-        
+                TestLab4.sortedSecondHalf.append(item)
         # print(f" {str(tempList)} from thread {threading.current_thread().name}")
 
     def mergingWorker(self) -> None:
@@ -59,71 +62,59 @@ class TestLab4:
             them into a single sorted list that is stored in
             the shared variable sortedFullList.
         """
-        i = j = 0                                           # set loop variables to 0
-        size = len(self.sortedFirstHalf)                         # size is len of half array
+        i = j = 0                                                               # set loop variables to 0
+        size = len(TestLab4.sortedFirstHalf)                                    # size is len of half array
         
-        while i < size and j < size:                        # traverse both lists before running out of values to compare on one list
-            if self.sortedFirstHalf[i] < self.sortedSecondHalf[j]:    # compare two values of list    
-                self.SortedFullList.append(self.sortedFirstHalf[i])   # if value in the firsthalf list is less, add to fullsorted list
-                i += 1                                      # move on to the next value in the firsthalf list
+        while i < size and j < size:                                            # traverse both lists before running out of values to compare on one list
+            if TestLab4.sortedFirstHalf[i] < TestLab4.sortedSecondHalf[j]:      # compare two values of list    
+                TestLab4.sortedFullList.append(TestLab4.sortedFirstHalf[i])     # if value in the firsthalf list is less, add to fullsorted list
+                i += 1                                                          # move on to the next value in the firsthalf list
             else:                                           
-                self.SortedFullList.append(self.sortedSecondHalf[j])  # otherwise value of secondhalf list is smaller, add this value to full sorted list
-                j += 1                                      # move on to the next value in the secondhalf list
+                TestLab4.sortedFullList.append(TestLab4.sortedSecondHalf[j])    # otherwise value of secondhalf list is smaller, add this value to full sorted list
+                j += 1                                                          # move on to the next value in the secondhalf list
 
-        for item in self.sortedFirstHalf[i:]:                    # if items left in firstHalf, add to fullsorted list
-            self.SortedFullList.append(item)
-        for item in self.sortedSecondHalf[j:]:                   # otherewise add leftover items in secondhalf to fullsorted list
-            self.SortedFullList.append(item)
+        for item in TestLab4.sortedFirstHalf[i:]:                               # if items left in firstHalf, add to fullsorted list
+            TestLab4.sortedFullList.append(item)
+        for item in self.sortedSecondHalf[j:]:                                  # otherewise add leftover items in secondhalf to fullsorted list
+            TestLab4.sortedFullList.append(item)
         
-    def testFunction(self) -> None:
+    def testFunction(self) -> list:
         """ function to unit test 
         """
-        # t1 = threading.Thread(target = self.sortingWorker, kwargs={"firstHalf": True})
-        # t2 = threading.Thread(target = self.sortingWorker, kwargs={"firstHalf": False})
-        # t1.start()
-        # t2.start()
-        # t1.join()
-        # t2.join()
-        # t3 = threading.Thread(target = self.mergingWorker)
-        # t3.start()
-        # t3.join()
-        self.sortedFirstHalf(True)
-        self.sortedSecondHalf(False)
-        self.mergingWorker
-        print(self.sortedFirstHalf)
-        print(self.SortedFullList)
+        t1 = threading.Thread(target = self.sortingWorker, kwargs={"firstHalf": True})
+        t2 = threading.Thread(target = self.sortingWorker, kwargs={"firstHalf": False})
+        t3 = threading.Thread(target = self.mergingWorker)
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+        t3.start()
+        t3.join()
+        return TestLab4.sortedFullList                
 
-    def getSortedFullList(self):
-        return self.SortedFullList        
+class unitTest(unittest.TestCase):      
+    
+    # write unit tests here
+    
+    def test1(self):
+        testCase1 = [7, -1, 3, 2]
+        newTest = TestLab4(testCase1)
+        self.assertListEqual(sorted(testCase1), newTest.testFunction())
 
-# class unitTest(unittest.TestCase):
+    def test2(self):
+        testCase2 = [7, 3, 3, 2, 3, 1, 5, 8]
+        newTest2 = TestLab4(testCase2)
+        self.assertListEqual(sorted(testCase2), newTest2.testFunction())
 
-#     def test1(self):
-#         # instantiate a test of Lab4
-#         testList = [7, -1, 3, 2]
-#         newTest = TestLab4(testList)
-#         newTest.testFunction
-#         self.assertListEqual(sorted(testList), newTest.getSortedFullList)
+    def test3(self):
+        testCase3 = [7, -1, 3, 2, 1, 1, -5, 4]
+        newTest3 = TestLab4(testCase3)
+        self.assertListEqual(sorted(testCase3), newTest3.testFunction())
 
-#     # def test2(self):
-#     #     testCase2 = [7, 3, 3, 2, 3, 1, 5, 8]
-#     #     self.testFunction(testCase2)
-#     #     self.assertListEqual(sorted(testCase2), self.SortedFullList, "lists not equal")
-
-#     # def test3(self):
-#     #     testCase3 = [7, -1, 3, 2, 1, 1, -5, 4]
-#     #     self.testFunction(testCase3)
-#     #     self.assertListEqual(sorted(testCase3), self.SortedFullList, "lists not equal")
-
-#     # def test4(self):
-#     #     testCase4 = [7, -10, 0, 4, 2, -1]
-#     #     self.testFunction(testCase4)
-#     #     self.assertListEqual(sorted(testCase4), self.SortedFullList, "lists not equal")
+    def test4(self):
+        testCase4 = ['a', 'b', 'd', 'c']
+        newTest = TestLab4(testCase4)
+        self.assertListEqual(sorted(testCase4), newTest.testFunction())
 
 if __name__ == '__main__':
-    # unittest.main()
-    testList = [7, -1, 3, 2]
-    test = TestLab4(testList)
-    test.testFunction()
-    
-
+    unittest.main()
