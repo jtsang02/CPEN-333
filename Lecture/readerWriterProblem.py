ed55718763fb7a01856a34115337aa617a5d0810
jtsang02 <jtsang02@student.ubc.ca>
2022-04-22 22:52:37 -0800

    reader - writer problem

--- Lecture/readerWriterProblem.py
+++ Lecture/readerWriterProblem.py
@@ -7,29 +7,49 @@ import random
 
 wrt = thread.Semaphore()        # used by writers
 mutex = thread.Semaphore()      # used by readers
-readCount = 0                   # keep trakc of how many processse/threads are currently read
-
-# define writer function
-def writer():
-    while True:
-        wrt.acquire()
-        print("writer is writing!")     # writing is performed
-        wrt.release()
-        print()
+readCount = 0                   # keep track of how many processse/threads are currently read
+x = 0                           # shared memory
 
 # define reader function
 def reader():
-    while True:
+        global readCount
+        global x
+        print ("reader is reading")
         mutex.acquire()
         readCount += 1
-        if readCount == 1:
+        print ("readCount value is: ", readCount)                  
+        if (readCount == 1):
             wrt.acquire()
         mutex.release()
-        print("reader is reading")   #reading is performed
+        print ("Shared data, x:" , x)   #reading is performed
         mutex.acquire()
         readCount -= 1
-        if readCount == 0:
+        print ("readCount value is: ", readCount)   
+        if (readCount == 0):
             wrt.release()
         mutex.release()
 
+# define writer function
+def writer():
+        global readCount
+        global x
+        wrt.acquire()
+        print ("writer is writing")     # writing is performed
+        x += 1                          # write on shared memory
+        
+        print ("writer is releasing the lock")
+        wrt.release()
 
+# main function
+if __name__ == '__main__':
+    for i in range(0, 10):
+        randomNumber = random.randint(0, 100)   #Generate a Random number between 0 to 100
+        if (randomNumber < 50):
+            Thread1 = thread.Thread(target = reader)
+            Thread1.start()
+        else:
+            Thread2 = thread.Thread(target = writer)
+            Thread2.start()
+
+Thread1.join()
+Thread2.join()
